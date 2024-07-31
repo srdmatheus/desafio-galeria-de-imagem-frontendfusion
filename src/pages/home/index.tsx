7ec114a8ce1a518f69e901a2da64ef2fe012dd98
Matheus Ribeiro <srdmatheus@gmail.com>
2024-07-30 23:19:39 -0300

    feat: add home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { ImageList } from "../../components/image-list";
+import { ImageModel } from "../../models/images";
+
+const fetchImages = async (): Promise<ImageModel[]> => {
+  const response = await fetch("https://picsum.photos/v2/list");
+  return response.json();
+};
+
 export const HomePage = () => {
-  return <div>home page</div>;
+  const { data, isLoading, error } = useQuery<ImageModel[]>({
+    queryKey: ["images"],
+    queryFn: fetchImages
+  });
+
+  if (error) {
+    return <div>Erro ao buscar imagens</div>;
+  }
+  return (
+    <main className="container mx-auto">
+      <h1 className="pb-6 pt-10 text-2xl font-bold tracking-tight">Início</h1>
+      {isLoading ? <p>Carregando...</p> : data && <ImageList images={data} />}
+    </main>
+  );
 };
