a4b8833d7191fd466394d975799545a7ddcf4d7f
Matheus Ribeiro <srdmatheus@gmail.com>
2024-07-30 23:17:29 -0300

    feat: add footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,19 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full border-t border-violet-400 bg-violet-100 py-10">
+      <div className="container mx-auto flex justify-center">
+        <p>
+          Feito por{" "}
+          <a
+            href="https://github.com/srdmatheus"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-violet-600 hover:underline"
+          >
+            Matheus Ribeiro
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
