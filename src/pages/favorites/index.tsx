438b0d49b666ed3db5617703edc3cf34caff0a11
Matheus Ribeiro <srdmatheus@gmail.com>
2024-07-30 23:19:30 -0300

    feat: add favorites page

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,7 +1,21 @@
+import { ImageList } from "../../components/image-list";
+import { useFavoriteStore } from "../../store/favorites-store";
+
 export const FavoritesPage = () => {
+  const favorites = useFavoriteStore((state) => state.favorites);
+
   return (
-    <main>
-      <h2>favorites</h2>
+    <main className="container mx-auto">
+      <h1 className="pb-6 pt-10 text-2xl font-bold tracking-tight">
+        Favoritos
+      </h1>
+      <div>
+        {favorites.length ? (
+          <ImageList images={favorites} />
+        ) : (
+          <p>Você não adicionou nenhuma imagem favorita.</p>
+        )}
+      </div>
     </main>
   );
 };
