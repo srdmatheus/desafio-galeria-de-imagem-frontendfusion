0e2fd206df7e7bdd833942588c387a7d43e2fdb1
Matheus Ribeiro <srdmatheus@gmail.com>
2024-08-01 22:28:36 -0300

    feat: add autor search to home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { ImageList } from "../../components/image-list";
 import { ImageModel } from "../../models/images";
+import { useState } from "react";
+import { Input } from "../../components/input";
 
 const fetchImages = async (): Promise<ImageModel[]> => {
   const response = await fetch("https://picsum.photos/v2/list");
@@ -13,13 +15,38 @@ export const HomePage = () => {
     queryFn: fetchImages
   });
 
+  const [search, setSearch] = useState<string | undefined>(undefined);
+
+  const filteredImages = data
+    ? data.filter((image) =>
+        search
+          ? image.author.toLowerCase().includes(search.toLowerCase())
+          : true
+      )
+    : [];
+
   if (error) {
     return <div>Erro ao buscar imagens</div>;
   }
   return (
     <main className="container mx-auto">
       <h1 className="pb-6 pt-10 text-2xl font-bold tracking-tight">Início</h1>
-      {isLoading ? <p>Carregando...</p> : data && <ImageList images={data} />}
+      <div className="flex w-96 flex-col pb-4">
+        <label htmlFor="author" className="font-medium">
+          Pesquisa por autor
+        </label>
+        <Input
+          type="text"
+          id="author"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      {isLoading ? (
+        <p>Carregando...</p>
+      ) : (
+        data && <ImageList images={filteredImages} />
+      )}
     </main>
   );
 };
