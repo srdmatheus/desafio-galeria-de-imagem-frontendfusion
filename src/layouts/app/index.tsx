0d44d18ffc75c39a39e20cbf35c30175f545c04c
Matheus Ribeiro <srdmatheus@gmail.com>
2024-08-20 21:52:14 -0300

    chore: change footer

--- src/layouts/app/index.tsx
+++ src/layouts/app/index.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../components/header";
-import { Footer } from "../../components/footer";
 
 export const AppLayout = () => {
   return (
@@ -9,7 +8,6 @@ export const AppLayout = () => {
       <div className="flex-1 px-4 pb-5 pt-20 xl:px-0">
         <Outlet />
       </div>
-      <Footer />
     </div>
   );
 };
